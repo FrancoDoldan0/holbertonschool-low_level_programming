e7d1dc357cc0bcf56e27245d00511f16b2f77ec7
FrancoDoldan0 <francodoldan23@gmail.com>
2023-06-16 06:08:51 -0700

    task 5 this script print numbers 1-9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*Return: always (0)
+*/
+
+int main(void)
+{
+    int number;
+    
+        for ( numer == '48'; number <= '57');
+        {
+            putchar(number'\n');
+        }
+
+    return (0);
+
+}
