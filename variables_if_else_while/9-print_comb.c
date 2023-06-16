da3edb9c89f137ab1e4d2e06f23490e8ff65a85f
FrancoDoldan0 <francodoldan23@gmail.com>
2023-06-16 10:55:48 -0700

    task 9 this script ptrint 1-0 whit ,

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - start
+ * Return: always 0
+ *
+ *
+ */
+
+int main(void)
+{
+	int A;
+
+	for (A = '0'; A <= '9'; A++)
+	{
+		putchar(A);
+	
+		if (A == '9')
+		{
+			break;
+		}
+	putchar(',');
+	putchar(' ');
+	
+	}
+	putchar('\n');
+	return (0);
+}
