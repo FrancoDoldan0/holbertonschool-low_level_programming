ec16194368e2ef0288a4ee725619192034961681
FrancoDoldan0 <francodoldan23@gmail.com>
2023-06-16 08:04:03 -0700

    task 7 this script print the alphabet in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always (0)
+ *
+ **/
+
+int main(void)
+{
+	char e;
+
+	for (e = 'a'; e <= 'z'; e--)
+	{
+
+	putchar(e);
+
+	}
+	putchar('\n');
+
+	return (0);
+}
