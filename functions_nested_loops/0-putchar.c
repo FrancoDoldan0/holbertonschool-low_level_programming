dfb7b2a662030f0bb57b5d373584ec383756d46c
FrancoDoldan0 <francodoldan23@gmail.com>
2023-06-16 19:37:31 -0700

    task 0 this program print _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
-
+#include "main.h"
+#include <stdlib.h>
 /**
-*main - entry point
-*
-*
-*Return: always (0)
-*
-*/
-
+ * main - start
+ * Return: always 0
+ *
+ *
+ */
 int main(void)
 {
-	char n[] = "_putchar";
-	int l = sizeof(n) - 1;
-	int e;
+	_putchar ('_');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	_putchar('\n');
 
-	for (e = 0; e < l; e++)
-	{
-	putchar(n[e]);
-	}
 	return (0);
 }
---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*main - entry point
+*
+*Return: always (0)
+*/
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	_putchar(i);
+	}
+
+	_putchar('\n');
+	return (0);
+}
