194a0f1eaefd87eb53808eb428d614aad7444a6c
FrancoDoldan0 <francodoldan23@gmail.com>
2023-06-19 13:43:17 -0700

    prueba

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
 /**
-*main - satrt
+* main - entry point
 *
-*Return: always (0)
+*
+* Return: always (0)
 *
 */
 
 int main(void)
 {
-printf("_putchar");
+char n[] = "_putchar";
+    int l = sizeof(n)-1;
 
+for (int e = 0; e < l; e++)
+{
+putchar(n[e]);
+putchar('\n');
+}
 return (0);
 }
