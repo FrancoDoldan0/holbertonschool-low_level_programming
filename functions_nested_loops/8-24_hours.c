4f809e90c1d84866a2e3e2f6f10d6ddc2e17dcd3
FrancoDoldan0 <francodoldan23@gmail.com>
2023-06-19 11:18:42 -0700

    task 8 this program prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * jack_bauer - print the watch of jack_bauer
+ *
+ */
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			putchar('\n');
+		}
+	}
+}
