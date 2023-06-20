1ff0aced0e10b2f16c6943fbe560572aa24bdbbc
FrancoDoldan0 <francodoldan23@gmail.com>
2023-06-20 16:49:00 -0700

    task 8 this program prints a square, followed by a new line.

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+void
+print_square(int size)
+{
+	int i, n;
+
+	if (size < 0)
+	{
+		for (i = 0; i <= size; i++)
+		{
+			for (n = 0; n < size; n++)
+			{
+				_putchar(32);
+				_putchar(35);
+			}
+		}
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
