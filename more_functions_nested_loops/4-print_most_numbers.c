1c0162f26c213dfe3c417ed5bd484d19ec60491c
FrancoDoldan0 <francodoldan23@gmail.com>
2023-06-20 09:24:52 -0700

    task 4 this program Write a function that prints the numbers, from 0 to 9, followed by a new line.

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -8,8 +8,11 @@ print_most_numbers(void)
 
 	for (n = '0'; n <= '9'; n++)
 	{
-		_putchar (n);
-		_putchar ('\n');
+		if (n != '2' && n != '4')
+		{
+			_putchar(n);
+		}
+		_putchar('\n');
 	}
 
 }
