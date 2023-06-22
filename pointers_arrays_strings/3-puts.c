6da6ce5c482c474837c0524bd1716bce96f1206e
FrancoDoldan0 <francodoldan23@gmail.com>
2023-06-21 17:37:23 -0700

    task 3 Write a function that prints a string, followed by a new line, to stdout.

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - Returns the length of a string.
+ * @str: The input string.
+ *
+ * Return: The length of the string.
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
