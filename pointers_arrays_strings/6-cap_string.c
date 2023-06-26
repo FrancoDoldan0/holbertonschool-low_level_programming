9243514c4a15c849223bfe5ce83d7ed66a2f7865
FrancoDoldan0 <francodoldan23@gmail.com>
2023-06-26 14:33:58 -0700

    task 6 Write a function that capitalizes all words of a string.

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include <ctype.h>
+
+char
+*cap_string(char *str)
+{
+	int capitalize_next = 1;
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (isspace(*ptr) || ispunct(*ptr))
+		{
+			capitalize_next = 1;
+		}
+		else if (capitalize_next)
+		{
+			*ptr = toupper(*ptr);
+			capitalize_next = 0;
+		}
+
+		ptr++;
+	}
+
+	return (str);
+}
