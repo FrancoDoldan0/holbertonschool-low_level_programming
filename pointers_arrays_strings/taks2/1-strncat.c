ed944564b7010a9b4adce820c3d6aeb2015aad2e
FrancoDoldan0 <francodoldan23@gmail.com>
2023-06-26 10:20:48 -0700

    task 1 Write a function that concatenates two strings.

---
+++ pointers_arrays_strings/taks2/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+char
+*_strncat(char *dest, char *src, int n)
+{
+	int a = 0;
+	int b = 0;
+
+	while (dest[a])
+	{
+		a++;
+	}
+
+	while (b < n)
+	{
+		dest[a] = src[b];
+		a++;
+		if (src[b] == '\0')
+		{
+			break;
+		}
+		b++;
+	}
+
+	dest[a] = '\0';
+
+	return (dest);
+}
