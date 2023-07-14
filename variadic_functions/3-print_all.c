2c7084de833f6c7b10ddc94cb0559418ecb4d02c
FrancoDoldan0 <francodoldan23@gmail.com>
2023-07-13 19:15:09 -0700

    task 3 Write a function that prints anything.

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -5,47 +5,55 @@
 void
 print_all(const char * const format, ...)
 {
-	va_list args;
-	unsigned int i = 0;
-	char *str;
-	int print_separator = 0;
+    va_list args;
+    unsigned int i = 0;
+    char *str;
+    int num;
+    char ch;
+    float f;
+    int is_valid_format = 1;
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (format && format[i])
-	{
-		if (format[i] == 'c')
-		{
-			printf("%c", va_arg(args, int));
-			print_separator = 1;
-		}
-		else if (format[i] == 'i')
-		{
-			printf("%d", va_arg(args, int));
-			print_separator = 1;
-		}
-		else if (format[i] == 'f')
-		{
-			printf("%f", va_arg(args, double));
-			print_separator = 1;
-		}
-		else if (format[i] == 's')
-		{
-			str = va_arg(args, char *);
-			printf("%s", (str != NULL) ? str : "(nil)");
-			print_separator = 1;
-		}
+    while (format && format[i])
+    {
+        is_valid_format = 1;
 
-		i++;
+        if (format[i] == 'c')
+        {
+            ch = va_arg(args, int);
+            printf("%c", ch);
+        }
+        else if (format[i] == 'i')
+        {
+            num = va_arg(args, int);
+            printf("%d", num);
+        }
+        else if (format[i] == 'f')
+        {
+            f = va_arg(args, double);
+            printf("%f", f);
+        }
+        else if (format[i] == 's')
+        {
+            str = va_arg(args, char *);
+            if (str != NULL)
+                printf("%s", str);
+            else
+                printf("(nil)");
+        }
+        else
+        {
+            is_valid_format = 0;
+        }
 
-		if (format[i] && print_separator)
-		{
-			printf(", ");
-			print_separator = 0;
-		}
-	}
+        if (format[i + 1] != '\0' && is_valid_format)
+            printf(", ");
 
-	va_end(args);
+        i++;
+    }
 
-	printf("\n");
+    va_end(args);
+
+    printf("\n");
 }
