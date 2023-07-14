c2df7e3577189aca10a4a3f9cf56ea931343ed3b
FrancoDoldan0 <francodoldan23@gmail.com>
2023-07-13 19:22:00 -0700

    task 3 Write a function that prints anything.

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,59 +1,68 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include "variadic_functions.h"
+#include<stdio.h>
+#include<stdarg.h>
 
 void
 print_all(const char * const format, ...)
 {
-    va_list args;
-    unsigned int i = 0;
-    char *str;
-    int num;
-    char ch;
-    float f;
-    int is_valid_format = 1;
 
-    va_start(args, format);
+	int i = 0, a, c;
+	double b;
+	char *d;
+	va_list list;
 
-    while (format && format[i])
-    {
-        is_valid_format = 1;
+	va_start(list, format);
 
-        if (format[i] == 'c')
-        {
-            ch = va_arg(args, int);
-            printf("%c", ch);
-        }
-        else if (format[i] == 'i')
-        {
-            num = va_arg(args, int);
-            printf("%d", num);
-        }
-        else if (format[i] == 'f')
-        {
-            f = va_arg(args, double);
-            printf("%f", f);
-        }
-        else if (format[i] == 's')
-        {
-            str = va_arg(args, char *);
-            if (str != NULL)
-                printf("%s", str);
-            else
-                printf("(nil)");
-        }
-        else
-        {
-            is_valid_format = 0;
-        }
-
-        if (format[i + 1] != '\0' && is_valid_format)
-            printf(", ");
-
-        i++;
-    }
-
-    va_end(args);
-
-    printf("\n");
+	while (format != NULL && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				a = va_arg(list, int);
+				print_c(a);
+				break;
+			case 'f':
+				b = va_arg(list, double);
+				print_f(b);
+				break;
+			case 'i':
+				c = va_arg(list, int);
+				 print_i(c);
+				break;
+			case 's':
+				 d = va_arg(list, char *);
+				 print_s(d);
+				break;
+		}
+		if (format[i] != '\0' &&
+				(format[i] == 'c' || format[i] == 'f' ||
+				 format[i] == 'i' || format[i] == 's'))
+		{
+			if (format[i + 1] != '\0')
+				printf(", ");
+		}
+		i++;
+	}
+	va_end(list);
+	printf("\n");
+}
+void
+print_c(char a)
+{
+	printf("%c", a);
+}
+void
+print_i(int a)
+{
+	printf("%d", a);
+}
+void
+print_f(float a)
+{
+	printf("%f", a);
+}
+void
+print_s(char *a)
+{
+	printf("%s", (a != NULL) ? a : "(nil)");
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,13 +1,22 @@
-#ifndef variadic_functions_h
-#define variadic_functions_h
-#include <stdlib.h>
-#include <stdarg.h>
-#include <stdio.h>
+#ifndef _variadic_functions_
+#define _variadic_functions_
 
-int _putchar(char c);
+#include<stdio.h>
+#include<stdarg.h>
+
+	/*task 0*/
 int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
 
+	/*task 1*/
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+	/*task 2*/
+void print_strings(const char *separator, const unsigned int n, ...);
+
+	/*task 3*/
+void print_all(const char * const format, ...);
+void print_c(char a);
+void print_i(int a);
+void print_f(float a);
+void print_s(char *a);
 #endif
