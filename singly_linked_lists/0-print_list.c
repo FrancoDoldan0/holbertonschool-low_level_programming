cf6271e4355603d17202ef68d782ad68ce2ae82f
FrancoDoldan0 <francodoldan23@gmail.com>
2023-07-25 18:22:28 -0700

    task 0 Write a function that prints all the elements of a list_t list.

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists_h"
+
+size_t print_list(const list_t *h)
+{
+	/*Se crea una variable llamada 'count' para contar los nodos en la lista.*/
+	size_t count = 0;
+
+	/*Se inicia un bucle 'while' que recorre la lista mientras el puntero 'h' no sea NULL.*/
+	while (h != NULL)
+	{	
+		/*Dentro del bucle, se verifica si el campo 'str' del nodo actual es NULL.*/
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		/*Se actualiza el puntero 'h' para apuntar al siguiente nodo en la lista.*/
+		h = h->next;
+
+		/*Se incrementa el contador de nodos*/
+		count++;
+	}
+	/*La función devuelve el valor de 'count', que representa la cantidad de nodos en la lista.*/
+	return (count);
+}
---
+++ singly_linked_lists/README.md
@@ -0,0 +1,16 @@
+<h1 align ="center">
+<img src="https://holbertonschool.uy/wp-content/themes/holberton/assets/img/logo.png" height="60%" width="50%">
+</h1>
+
+<h1>Singly linked lists<h1>
+
+<h2>What is single linked lists<h2>
+
+<p>A singly linked list is a linear data structure in which the elements are not stored in contiguous memory locations and each element is connected only to its next element using a pointer.
+
+<h2>Learning Objectives<h2>
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+
+<h2>General<h2>
+<li>When and why using linked lists vs arrays
+<li>How to build and use linked lists
