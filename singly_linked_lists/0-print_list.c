33ccf62b1ae26e796a38bd1e71298a62f3c50f75
FrancoDoldan0 <francodoldan23@gmail.com>
2023-07-25 18:27:08 -0700

    task 0 Write a function that prints all the elements of a list_t list.

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,13 +1,20 @@
 #include "lists_h"
+/**
+  * print_list - Prints all elements of a list
+  * @h: A linked list
+  *
+  * Return: The number of nodes
+  */
 
 size_t print_list(const list_t *h)
 {
 	/*Se crea una variable llamada 'count' para contar los nodos en la lista.*/
 	size_t count = 0;
 
-	/*Se inicia un bucle 'while' que recorre la lista mientras el puntero 'h' no sea NULL.*/
+	/*Se inicia un bucle 'while' que recorre la lista*/
+		/*mientras el puntero 'h' no sea NULL.*/
 	while (h != NULL)
-	{	
+	{
 		/*Dentro del bucle, se verifica si el campo 'str' del nodo actual es NULL.*/
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
@@ -20,6 +27,7 @@ size_t print_list(const list_t *h)
 		/*Se incrementa el contador de nodos*/
 		count++;
 	}
-	/*La función devuelve el valor de 'count', que representa la cantidad de nodos en la lista.*/
+	/*La función devuelve el valor de 'count',*/
+	/*que representa la cantidad de nodos en la lista.*/
 	return (count);
 }
