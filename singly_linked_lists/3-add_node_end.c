636a86f524b84d3d4c63bd3f1fccc8bb39268be1
FrancoDoldan0 <francodoldan23@gmail.com>
2023-07-27 13:37:11 -0700

    task 3 Write a function that adds a new node at the end of a list_t list.

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,52 +1,53 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * add_node_end - función que agrega un nuevo nodo al final de la lista.
- * @head: Puntero al puntero del inicio de la lista.
- * @str: Cadena a almacenar en el nuevo nodo.
- *
- * Return: Puntero al nuevo nodo.
- */
+*add_node_end - Agrega un nuevo nodo al final de la lista.
+* @head: Puntero al puntero que apunta a la cabeza de la lista.
+* @str: Cadena que se va a almacenar en el nuevo nodo.
+* Return: Puntero al último nodo de la lista. *
+*/
 list_t *add_node_end(list_t **head, const char *str)
 {
-	/*Declarar un puntero para el nuevo nodo*/
+	/* Puntero temporal para recorrer la lista y encontrar el último nodo. */
 	list_t *lastnode = *head;
-	list_t *new = NULL;
+	list_t *new = NULL; /* Nuevo nodo que se va a añadir al final. */
 
-	/*Asignar memoria para el nuevo nodo*/
+	/* Asignación de memoria para el nuevo nodo. */
 	new = (list_t *)malloc(sizeof(list_t));
-
-	/*Comprobar si la asignación de memoria fue exitosa*/
 	if (new == NULL)
 	{
-		/*Liberar la memoria y retornar NULL en caso de error*/
+	/* Si la asignación de memoria falla,*/
+/*liberamos el espacio previamente asignado. */
 		free(new);
+		/* Retornamos NULL para indicar el fallo en la creación del nuevo nodo. */
 		return (NULL);
 	}
 
-	/*Duplicar la cadena y almacenar su longitud en el nuevo nodo*/
+/* Copiamos la cadena proporcionada al nuevo nodo. */
 	new->str = strdup(str);
+	/* Obtenemos la longitud de la cadena y la almacenamos en el nuevo nodo. */
 	new->len = strlen(str);
 
-	/*Establecer el puntero "next" del nuevo nodo*/
-	/*para que apunte al nodo principal actual*/
-	new->next = *head;
-
-		/*El nuevo nodo se convierte en el nodo principal,*/
-	/*actualizamos el puntero head para que apunte al nuevo nodo*/
-	*head = new;
-
-	/*Devolver el puntero al nuevo nodo*/
-	return (*head);
-
-/*Recorre la lista hasta el último nodo*/
-	while (lastnode->next != NULL)
+	if (*head == NULL)
 	{
-		lastnode = lastnode->next;
+					/* Si la lista está vacía*/
+		/*el nuevo nodo se convierte en la cabeza de la lista. */
+		*head = new;
+		return (*head);
 	}
 
-/*Establece el puntero next del último nodo al nuevo nodo*/
+	while (lastnode->next != NULL)
+	{
+/* Recorremos la lista hasta encontrar el último nodo. */
+		lastnode = lastnode->next;
+	}
+/* Establecemos el puntero "next" del último nodo al nuevo nodo*/
+				/*añadiéndolo al final. */
 	lastnode->next = new;
 
+/* Retornamos el puntero al último nodo (que ahora es el nuevo nodo). */
 	return (lastnode->next);
 }
