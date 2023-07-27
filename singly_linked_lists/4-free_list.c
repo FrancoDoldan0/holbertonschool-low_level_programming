5f2d1e8168a13776755f870a61b2b644d231a7c9
FrancoDoldan0 <francodoldan23@gmail.com>
2023-07-27 14:11:18 -0700

    task 4 Write a function that frees a list_t list.

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,22 +1,30 @@
 #include "lists.h"
+
 /**
-*free_list - Function that free a list
-*
-*@head: head
-*
-*/
+ * free_list - Libera la memoria ocupada por una lista.
+ * @head: Puntero al primer nodo de la lista.
+ *
+ * Esta función recorre la lista enlazada y libera la memoria
+ * ocupada por cada nodo, incluyendo las cadenas almacenadas
+ * en cada nodo.
+ */
 void free_list(list_t *head)
 {
-	if (head != NULL)
+	/* Verifica si el puntero a la cabeza de la lista es NULL (lista vacía) */
+	if (head == NULL)
 	{
-		free(head);
-		return;
+		free(head); /* Libera el nodo de cabeza (que es NULL en este caso) */
+		return; /* Sale de la función, ya que no hay más nodos que liberar */
 	}
-free_list(head->next);
 
+	/* Llamada recursiva a la función para liberar el resto de la lista */
+	free_list(head->next);
+
+/*Verifica si el puntero a la str almacenada en el nodo es diferente de NULL*/
 	if (head->str)
 	{
-		free(head->str);
+		free(head->str);/*Libera la memoria ocupada por la cadena en el nodo actual*/
 	}
-	free(head);
+
+	free(head); /* Finalmente, libera la memoria ocupada por el nodo actual */
 }
